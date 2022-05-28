f612cf52a4e060d1fe5e498f4570899ccd2e67ab
YablokovaValeriya <99179279+YablokovaValeriya@users.noreply.github.com>
2022-05-28 22:59:17 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,63 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+template <typename T>
+class BST {
+ public:
+  struct Node {
+    T value;
+    int count;
+    Node* right;
+    Node* left;
+  };
+BST(): root(nullptr) {}
+~BST() {}
+void add(T value) {
+  root = addNode(root, value);
+}
+int height() {
+  return heightTree(root);
+}
+
+ private:
+  Node* root;
+  Node* addNode(Node* root, T value) {
+    if (root == nullptr) {
+      root = new Node;
+      root->value = value;
+      root->count = 1;
+      root->left = root->right = nullptr;
+    } else if (root->value > value) {
+      root->left = addNode(root->left, value);
+    } else if (root->value < value) {
+      root->right = addNode (root->right, value);
+    } else {
+      root->count++;
+    }
+  int heightTree(Node* root) {
+    if (root == nullptr)
+      return 0;
+    if ((root->left == nullptr) && (root->right == nullptr)) {
+      return 0;
+    }
+    
+    int L = heightTree(root->left);
+    int R = heightTree(root->right);
+    if (R>L)
+      return R+1;
+    else return L+1;
+    
+  }
+  int searchNode(Node* root, T value) {
+    if (root == nullptr)
+      return 0;
+    else if (root->value > value)
+      return searchNode(root->left, value);
+    else if (root->value < value)
+      return searchNode(root->right, value);
+    else
+      return root->count;
+  }
+};
 
 #endif  // INCLUDE_BST_H_
